575090c54a6c8fa5f0dd3644103bf163fd9fbdb2
Ueverton Passos <uevertonpassos96@gmail.com>
2023-10-16 16:49:00 -0300

    Finalização do desafio

---
+++ escalas.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	k := 373.15
+	c := k - 273.15
+
+	fmt.Printf("A temperatura da ebulição da água transformada de Kelvin para Celsius é de: %.2f", c)
+}
